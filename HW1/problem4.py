9b4fe785629ecfdf5eaf62873cc625d32d072a48
khachatur98 <khachatur1998@gmail.com>
2021-02-21 00:33:21 +0400

    Printed value before the changes

--- HW1/problem4.py
+++ HW1/problem4.py
@@ -1,9 +1,9 @@
 market = {'dairy': ['yogurt','cheese'],
           'fruits': ['banana', 'apple', 'orange', 'lemon', 'apple', 'banana','banana']}
+print(market)
 
 market['candies'] =  ['mars', 'kinder', 'twix']
 
 market['fruits'] = sorted(list(set(market['fruits'])))
-
 
 print(market)
